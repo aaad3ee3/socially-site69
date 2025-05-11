2b71713c6acd93cc41a3436b4ad87ca527f2a936
Abdrhman <xgxyxhdhs@gmail.com>
2025-05-11 14:36:48 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontSize: '24px' }}>
+      مرحبًا بك في موقع <strong>سوشيالي</strong> 🚀<br />
+      اطلب متابعين، لايكات، مشاهدات، وأكثر!
+    </div>
+  );
+}
+
+export default App;
